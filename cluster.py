3f6e98b27a99567d33ea11f6026b9fb9193c1396
Dylan Randle <dylanrandle@rogers.com>
2018-09-20 18:35:33 -0400

    remove unimplemented cluster.py

--- cluster.py
+++
@@ -1,8 +0,0 @@
-## Clustering algorithms
-
-input = [list(map(int, x.split(' '))) for x in open('data/clustering1.txt', 'r').read().split('\n')[1:-1]]
-
-def basic_greedy_max_spacing(input, k):
-    num_clusters = len(input)
-    while num_clusters > k:
-        
